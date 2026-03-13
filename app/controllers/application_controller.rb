18e2df887efe0f224af064beae009440d6cd82a6
Steph Coffey <stephcoffey@Stephs-MacBook-Pro.local>
2026-03-13 15:49:10 -0600

    add authenticate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,9 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  http_basic_authenticate_with(
+    name: ENV['APP_USER'] || 'admin',
+    password: ENV['APP_PASS'] || 'secret'
+  )
 end
